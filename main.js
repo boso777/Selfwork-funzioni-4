b445b79464bcdc95c636453f10b53f1c86ed81ef
Boso Mac <robertoingrao4@gmail.com>
2026-01-29 14:39:19 +0100

    start

---
+++ main.js
@@ -0,0 +1,7 @@
+// Scrivi una funzione che prenda in input una stringa e restituisca TRUE se è palindroma, FALSE se non lo è.
+// Primo step: eliminare gli spazi e i segni di punteggiatura:
+// Suggerimento: Puoi eliminare spazi e segni di punteggiatura usando → str.replace(/\W/g, "")
+
+// Esempio:
+// Input: “i topi non avevano nipoti”
+// Output: true
